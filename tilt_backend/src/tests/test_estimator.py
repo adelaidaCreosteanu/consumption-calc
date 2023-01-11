84586cdcede007a429c399ac3574c91ed547ad07
Heidi <adelaida.creosteanu@gmail.com>
2023-01-11 19:52:25 +0200

    refactor: use pytest fixtures

--- tilt_backend/src/tests/test_estimator.py
+++ tilt_backend/src/tests/test_estimator.py
@@ -9,24 +9,39 @@ from src.types import Appliance, UsageCategory
 from src.estimator import estimate, compute_min_max_consumption
 
 
-def test_compute_min_max_consumption():
-    f = Appliance("Fridge", UsageCategory.F, power=2)
-    dwr = Appliance("Dishwasher", UsageCategory.A, power=2.5)
-    tv = Appliance("TV", UsageCategory.L, power=0.5)
-    ist = Appliance("Induction stove", UsageCategory.A, power=3)
-    lt = Appliance("Big light", UsageCategory.L, power=0.8)
+@pytest.fixture(scope="session")
+def appliances():
+    return {
+        "f": Appliance("Fridge", UsageCategory.F, power=2),
+        "dwr": Appliance("Dishwasher", UsageCategory.A, power=2.5),
+        "tv": Appliance("TV", UsageCategory.L, power=0.5),
+        "ist": Appliance("Induction stove", UsageCategory.A, power=3),
+        "lt": Appliance("Big light", UsageCategory.L, power=0.8),
+    }
 
-    actual = compute_min_max_consumption([f, dwr])
-    assert actual["min"] == 14.5
-    assert actual["max"] == 26.0
 
-    actual = compute_min_max_consumption([tv, ist, lt])
-    assert actual["min"] == 5.6
-    assert actual["max"] == 27.6
+@pytest.fixture(params=[2, 3, 5])
+def appliance_group(appliances, request):
+    number = request.param
+    aplcs = [appliances["f"], appliances["dwr"]]
+    if number == 2:
+        return aplcs
+    aplcs.append(appliances["tv"])
+    if number == 3:
+        return aplcs
+    aplcs.extend([appliances["ist"], appliances["lt"]])
+    return aplcs
 
-    actual = compute_min_max_consumption([f, dwr, tv, ist, lt])
-    assert actual["min"] == 17.35
-    assert actual["max"] == 42.6
+
+@pytest.mark.parametrize(
+    "appliance_group, ex_min, ex_max",
+    [(2, 14.5, 26.0), (3, 16.5, 38.0), (5, 17.35, 42.6)],
+    indirect=["appliance_group"],
+)
+def test_compute_min_max_consumption(appliance_group, ex_min, ex_max):
+    actual = compute_min_max_consumption(appliance_group)
+    assert actual["min"] == ex_min
+    assert actual["max"] == ex_max
 
 
 @pytest.mark.parametrize("category",
@@ -45,65 +60,51 @@ def test_estimate_single_appliances(category):
     assert actual[a.name] == expected
 
 
-def test_estimate_two_categories():
-    f = Appliance("Fridge", UsageCategory.F, power=2)
-    tv = Appliance("TV", UsageCategory.L, power=0.5)
-    consumption = 21
-    # expected ranges for consumption
-    expected_fridge = {"range": [12.0, 16.0], "mean": 14.0}
-    expected_tv = {"range": [5.0, 9.0], "mean": 7.0}
+@pytest.mark.parametrize(
+    "appliance_group, expected_vals",
+    [
+        # Two appliances
+        (2, [{
+            "range": [12.0, 16.0],
+            "mean": 14.0
+        }, {
+            "range": [4.0, 8.0],
+            "mean": 6.0
+        }]),
+        # Three appliances
+        (3, [{
+            "range": [12.0, 15.5],
+            "mean": 13.75
+        }, {
+            "range": [2.5, 6.0],
+            "mean": 4.25
+        }, {
+            "range": [2.0, 5.5],
+            "mean": 2.0
+        }]),
+        # Five appliances
+        (5, [{
+            "range": [12.0, 14.65],
+            "mean": 13.325
+        }, {
+            "range": [1.25, 2.454545454545455],
+            "mean": 1.8522727272727275
+        }, {
+            "range": [1.0, 2.019230769230769],
+            "mean": 1.0000000000000004
+        }, {
+            "range": [1.5, 2.9454545454545458],
+            "mean": 2.2227272727272727
+        }, {
+            "range": [1.6, 3.230769230769231],
+            "mean": 1.6000000000000008,
+        }])
+    ],
+    indirect=["appliance_group"],
+)
+def test_estimate_appliance_group(appliance_group, expected_vals):
+    actual = estimate(appliance_group, 20)
 
-    actual = estimate([f, tv], consumption)
-    assert actual[f.name] == expected_fridge
-    assert actual[tv.name] == expected_tv
-
-
-def test_estimate_three_categories():
-    f = Appliance("Fridge", UsageCategory.F, power=2)
-    dwr = Appliance("Dishwasher", UsageCategory.A, power=2.5)
-    tv = Appliance("TV", UsageCategory.L, power=0.5)
-    consumption = 20
-    # expected ranges for consumption
-    expected_fridge = {"range": [12.0, 15.5], "mean": 13.75}
-    expected_dwr = {"range": [2.5, 6.0], "mean": 4.25}
-    expected_tv = {"range": [2.0, 5.5], "mean": 2.0}
-
-    actual = estimate([f, dwr, tv], consumption)
-    assert actual[f.name] == expected_fridge
-    assert actual[dwr.name] == expected_dwr
-    assert actual[tv.name] == expected_tv
-
-
-def test_estimate_five_appliances():
-    f = Appliance("Fridge", UsageCategory.F, power=2)
-    dwr = Appliance("Dishwasher", UsageCategory.A, power=2.5)
-    tv = Appliance("TV", UsageCategory.L, power=0.5)
-    ist = Appliance("Induction stove", UsageCategory.A, power=3)
-    lt = Appliance("Big light", UsageCategory.L, power=0.8)
-
-    consumption = 20
-    # expected ranges for consumption
-    expected_fridge = {"range": [12.0, 14.65], "mean": 13.325}
-    expected_dwr = {
-        "range": [1.25, 2.454545454545455],
-        "mean": 1.8522727272727275
-    }
-    expected_tv = {
-        "range": [1.0, 2.019230769230769],
-        "mean": 1.0000000000000004
-    }
-    expected_ist = {
-        "range": [1.5, 2.9454545454545458],
-        "mean": 2.2227272727272727
-    }
-    expected_lt = {
-        "range": [1.6, 3.230769230769231],
-        "mean": 1.6000000000000008,
-    }
-
-    actual = estimate([f, dwr, tv, ist, lt], consumption)
-    assert actual[f.name] == expected_fridge
-    assert actual[dwr.name] == expected_dwr
-    assert actual[tv.name] == expected_tv
-    assert actual[ist.name] == expected_ist
-    assert actual[lt.name] == expected_lt
+    for i in range(len(appliance_group)):
+        name = appliance_group[i].name
+        assert actual[name] == expected_vals[i]
